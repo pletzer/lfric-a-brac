a6683953d5e60893a50da76aaec574da1fc08b41
Alex Pletzer <alexander@gokliya.net>
2022-08-12 11:17:11 +1200

    adding line averages

---
+++ lfric-a-brac/line_average_vector_field.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from extensive_field import ExtensiveField
+from polyline import Polyline
+from lateral_flux import LateralFlux
+import defopt
+import numpy
+
+
+def main(*, filename: Path='./lfric_diag.nc',
+            target_line: str='[(-180., -85.), (180., 85.)]'):
+
+    ef = ExtensiveField(filename=filename)
+    ef.build()
+    ef.compute_edge_integrals()
+
+    xy = numpy.array(eval(target_line))
+    line = Polyline(xy, planet_radius=1.0)
+    lengths = line.get_lengths()
+
+    lf = LateralFlux(ef)
+    lf.set_target_line(line)
+
+    lf.compute_fluxes()
+    fluxes = lf.get_fluxes()
+    print(f'line averages of the vector field: {fluxes/lengths}')
+
+
+if __name__ == '__main__':
+    defopt.run(main)
